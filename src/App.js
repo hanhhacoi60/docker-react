5d46175986817c9fc99a1d5b09b472a67b27c4e0
hanhhacoi60 <62678520+hanhhacoi60@users.noreply.github.com>
2020-05-16 02:36:58 +0700

    Deploy to Vercel

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hack there</p>
+        <p>Deploy to Vercel</p>
         <a
           className="App-link"
           href="https://reactjs.org"
